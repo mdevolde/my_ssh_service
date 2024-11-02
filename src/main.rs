43ff33973a4ef228ff693db94d019c1f78e5654e
mdevolde <martin.devolder@student.unamur.be>
2024-11-02 20:32:01 +0100

    Updating the service to start a ssh connection with some args passed to the service

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate windows_service;
 
 use std::ffi::OsString;
 use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
+use std::process::Command;
 use windows_service::{
     service_dispatcher, Result,
     service::{ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus, ServiceType},
@@ -11,28 +12,30 @@ use windows_service::{
 
 define_windows_service!(ffi_service_main, my_service_main);
 
-const SERVICE_NAME: &str = "myservicee";
+const SERVICE_NAME: &str = "mysshservice";
 
-fn my_service_main(_arguments: Vec<OsString>) {
-    if let Err(e) = run_service() {
+fn my_service_main(arguments: Vec<OsString>) {
+    if let Err(e) = run_service(arguments) {
         eprintln!("Service failed: {:?}", e);
     }
 }
 
-fn run_service() -> Result<()> {
+fn run_service(arguments: Vec<OsString>) -> Result<()> {
     let running = Arc::new(AtomicBool::new(true));
     let running_handle = running.clone();
 
+    // Register the service control handler
     let status_handle = service_control_handler::register(SERVICE_NAME, move |control_event| {
         match control_event {
             ServiceControl::Stop => {
-                running_handle.store(false, Ordering::SeqCst);
+                running_handle.store(false, Ordering::SeqCst); // Tell to the thread to stop when there is a stop event
                 ServiceControlHandlerResult::NoError
             },
             _ => ServiceControlHandlerResult::NotImplemented,
         }
     })?;
 
+    // Update the service status to Running
     status_handle.set_service_status(ServiceStatus {
         service_type: ServiceType::OWN_PROCESS,
         current_state: ServiceState::Running,
@@ -43,11 +46,38 @@ fn run_service() -> Result<()> {
         process_id: None,
     })?;
 
+    let running_clone = Arc::clone(&running);
+    // Start the SSH command in a separate thread
+    std::thread::spawn(move || {
+        let args: Vec<String> = arguments
+            .into_iter()
+            .skip(1) // Skip the first argument which is the service name
+            .map(|arg| arg.to_string_lossy().into_owned())
+            .collect();
+
+
+        if let Ok(mut child) = Command::new("ssh")
+            .args(args)
+            .spawn()
+        {
+            // Wait until the service is stopped
+            while running_clone.load(Ordering::SeqCst) {
+                std::thread::sleep(std::time::Duration::from_secs(5));
+            }
+
+            // Stop the SSH process if it's still running
+            let _ = child.kill();
+        } else {
+            eprintln!("Error starting the SSH command");
+        }
+    });
+
+    // Wait until the service is stopped
     while running.load(Ordering::SeqCst) {
-        std::thread::sleep(std::time::Duration::from_secs(5));
-        std::fs::write("C:\\ProgramData\\service.txt", format!("{:?}", std::time::SystemTime::now())).unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 
+    // Update the service status to Stopped
     status_handle.set_service_status(ServiceStatus {
         service_type: ServiceType::OWN_PROCESS,
         current_state: ServiceState::Stopped,
